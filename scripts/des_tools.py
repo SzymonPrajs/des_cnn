b8a091deedd2afd161573e1ff8055ce3202a40c1
Szymon Prajs <S.Prajs@soton.ac.uk>
2017-05-16 20:19:44 +0100

    Begin testing efficient ways of reading in SNANA data files

---
+++ scripts/des_tools.py
@@ -0,0 +1,93 @@
+"""
+Miscellenious helper functions specifically for the use with DES
+"""
+import numpy as np
+
+
+def mjd_to_season(mjd):
+    """
+    Find a corresponding DES observing season for a given MJD
+
+    Properties
+    ----------
+    mjd : float
+        MJD of an observation
+
+    Returns
+    -------
+    season : int
+        Observing season corresponding to a given MJD
+    """
+    if 56000 < mjd < 56750:
+        return 1
+
+    elif 56750 < mjd < 57150:
+        return 2
+
+    elif 57150 < mjd < 57550:
+        return 3
+
+    elif 57550 < mjd < 57950:
+        return 4
+
+    elif 57950 < mjd < 58350:
+        return 5
+
+    else:
+        return 0
+
+
+def parse_status_code(status_code):
+    """
+    Parse DES image quality status code into an array of individual flags
+
+    Properies
+    ---------
+    status_code : int
+        DES image quality status code
+
+    Returns
+    -------
+    status_flags : `np.array`
+        Array of error flags
+    """
+    status_flags = np.zeros(14)
+    for i in range(13, -1, -1):
+        if status_code == status_code % 2**i:
+            status_flags[i] = 0
+
+        else:
+            status_flags[i] = 1
+
+        status_code = status_code % 2**i
+
+    return status_flags
+
+
+def get_status_quality(status_code):
+    """
+    Convert photometric quality status code into a simpler output:
+    0 : bad photometry
+    1 : good photometry
+    2 : good photometry + real-time detection
+
+    Properties
+    ----------
+    status_code : int
+        DES image quality status code
+
+    Returns
+    -------
+    status_quality : int
+        Quality flag
+    """
+    status_flags = parse_status_code(status_code)
+
+    if status_flags[3:-2].sum() > 0:
+        return 0
+
+    elif status_flags[-2:].sum() > 0:
+        return 2
+
+    else:
+        return 1
---
+++ scripts/read_snana_files.py
@@ -0,0 +1,59 @@
+"""
+Collection of useful tools used for reading and parsing various types of SNANA
+data files including DES SN light curve, DES fake SN light curves and SIMLIB
+files
+"""
+from .des_tools import mjd_to_season, get_status_quality
+
+
+def split_textfile_line(text_line):
+    """
+    Clear a line of text from spaces and linebreak characters the split to list
+    """
+    out = text_line.rstrip(' \n')
+    while '  ' in out:
+        out = out.replace('  ', ' ')
+    out = out.split(' ')
+
+    return out[0][:-1], out[1:]
+
+
+def parse_snana_line(snana_line):
+    """
+    Parse a line from SNANA
+    """
+    key, value = split_textfile_line(snana_line)
+
+    if key == '':
+        return None, value
+
+    if key == 'OBS':
+        value = value[:12]
+        value.append(mjd_to_season(float(value[0])))
+        value.append(get_status_quality(int(value[5])))
+
+    return key, value
+
+
+def read_fake_file(file_path):
+    """
+    Read a single fake file
+
+    Properties
+    ----------
+    file_path : str
+        Path to the fake SN data file
+    """
+    obs = []
+
+    with open(file_path, 'r') as fake_file:
+        for fake_line in fake_file:
+            key, value = parse_snana_line(fake_line)
+
+            if key == 'OBS':
+                obs.append(tuple(value))
+
+
+if __name__ == "__main__":
+    read_fake_file("/Users/szymon/Dropbox/Projects/DES/data/fakes/"
+                   "des_fake_00234999.dat")
