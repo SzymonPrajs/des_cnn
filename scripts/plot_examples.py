6d33c9a2e6ecf9f119855219762fef0daec508ae
Szymon Prajs <S.Prajs@soton.ac.uk>
2018-03-10 14:49:21 +0000

    Plot fake AGN and Ia example

--- scripts/plot_examples.py
+++ scripts/plot_examples.py
@@ -5,6 +5,7 @@ from tools.des_tools import query_localdb
 from tools.plot_des import plot_all_seasons
 
 
+"""   SLSN   """
 query = """
 WITH id AS (
     SELECT DISTINCT(snid) FROM slsn_5_realisations_2
@@ -20,3 +21,37 @@ print('Fake SLSN -  snid:', df['snid'][0], ' -  z:', df['z'][0])
 
 ax = plot_all_seasons(df)
 plt.savefig('/Users/szymon/Desktop/example_SLSN.png', bbox_inches='tight')
+
+
+"""   AGN   """
+query = """
+WITH id AS (
+    SELECT DISTINCT(snid) FROM agn_10_realisations
+    WHERE flux > 5000
+    ORDER BY snid ASC LIMIT 1
+)
+SELECT a.* FROM agn_10_realisations a
+JOIN id ON a.snid=id.snid
+"""
+df = query_localdb(query)
+print('Fake AGN -  snid:', df['snid'][0])
+
+ax = plot_all_seasons(df)
+plt.savefig('/Users/szymon/Desktop/example_AGN.png', bbox_inches='tight')
+
+
+"""   Ia   """
+query = """
+WITH id AS (
+    SELECT DISTINCT(snid) FROM fake_ia_obs
+    WHERE flux > 5000
+    ORDER BY snid ASC LIMIT 1
+)
+SELECT a.* FROM fake_ia_obs a
+JOIN id ON a.snid=id.snid
+"""
+df = query_localdb(query)
+print('Fake AGN -  snid:', df['snid'][0])
+
+ax = plot_all_seasons(df)
+plt.savefig('/Users/szymon/Desktop/example_SNIa.png', bbox_inches='tight')
