4de9c10b458a1d2efa2d2662cbb1114398ae70bd
Szymon Prajs <S.Prajs@soton.ac.uk>
2018-03-16 13:14:48 +0000

    Small bug fix

--- plot/plot_peak_lum.py
+++ plot/plot_peak_lum.py
@@ -17,18 +17,18 @@ GROUP BY snid
 df = pd.read_sql_query(query, engine)
 
 plt.cla()
-plt.scatter(df['z'], -2.5*np.log10(df['peak'] + 31.4))
+plt.scatter(df['z'], -2.5*np.log10(df['peak']) + 31.4)
 plt.savefig('/Users/szymon/Dropbox/Plots/SNIbc_peak_lum.png', bbox_inches='tight')
 
 
-# """   SLSN   """
-# query = """
-# SELECT snid, MAX(flux) AS peak, MAX(z) AS z
-# FROM slsn_5_realisations_2 WHERE flux/fluxerr > 8
-# GROUP BY snid
-# """
-# df = pd.read_sql_query(query, engine)
-#
-# plt.cla()
-# plt.scatter(df['z'], -2.5*np.log10(df['peak'] + 31.4))
-# plt.savefig('/Users/szymon/Dropbox/Plots/SLSN_peak_lum.png', bbox_inches='tight')
+"""   SLSN   """
+query = """
+SELECT snid, MAX(flux) AS peak, MAX(z) AS z
+FROM slsn_5_realisations_2 WHERE flux/fluxerr > 8
+GROUP BY snid
+"""
+df = pd.read_sql_query(query, engine)
+
+plt.cla()
+plt.scatter(df['z'], -2.5*np.log10(df['peak']) + 31.4)
+plt.savefig('/Users/szymon/Dropbox/Plots/SLSN_peak_lum.png', bbox_inches='tight')
