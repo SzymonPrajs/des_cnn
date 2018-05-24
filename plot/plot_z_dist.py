86253949d254fc91c7158e8f6ff0757b819c8c7f
Szymon Prajs <S.Prajs@soton.ac.uk>
2018-05-24 16:11:44 +0100

    Plot redshift for SNIa

--- plot/plot_z_dist.py
+++ plot/plot_z_dist.py
@@ -8,6 +8,21 @@ sql = 'postgresql://szymon:supernova@localhost:5432/thesis'
 engine = create_engine(sql)
 
 
+"""   Ia   """
+query = "SELECT distinct(snid), z FROM fake_ia_obs"
+df = pd.read_sql_query(query, engine)
+# z = np.linspace(0, 1.4, 1000)
+# pdf = dest.get_sfr_z_pdf(0.8, 0.01)
+
+plt.cla()
+# plt.plot(z, pdf(z), label='Input distribution')
+plt.hist(df['z'].values, bins=20, normed=1, label='Observed distribution')
+plt.xlabel('Redshift')
+
+plt.legend()
+plt.savefig('/Users/szymon/Dropbox/Plots/SNIa_z_dist.png', bbox_inches='tight')
+
+
 """   II   """
 query = "SELECT distinct(snid), z FROM fake_ii_obs"
 df = pd.read_sql_query(query, engine)
