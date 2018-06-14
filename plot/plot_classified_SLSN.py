1785fe1d5d54e33ae03518530991f7d76e39347d
Szymon Prajs <S.Prajs@soton.ac.uk>
2018-06-14 22:32:52 +0100

    Fix stuff

--- plot/plot_classified_SLSN.py
+++ plot/plot_classified_SLSN.py
@@ -10,10 +10,10 @@ from tools.plot_des import plot_all_seasons
 SLSN = np.loadtxt('/Users/szymon/Dropbox/SLSN_99.csv', unpack=True)
 engine = create_engine('postgresql://szymon:supernova@localhost:5432/thesis')
 
-for snid from SLSN:
-    query = f"SELECT * FROM real_des_obs_corr WHERE snid={snid}"
+for snid in SLSN:
+    query = "SELECT * FROM real_des_obs_corr WHERE snid="+str(snid)
     df = query_localdb(query)
     print('snid:', df['snid'][0])
 
     ax = plot_all_seasons(df)
-    plt.savefig(f'/Users/szymon/Dropbox/Plots/SLSN/{snid}.png', bbox_inches='tight')
+    plt.savefig(f'/Users/szymon/Dropbox/Plots/SLSN/'+str(snid)+'.png', bbox_inches='tight')
