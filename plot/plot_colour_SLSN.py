0a12460541fe1b174d770223697a6ded83a80149
Szymon Prajs <S.Prajs@soton.ac.uk>
2018-06-16 23:13:11 +0100

    Fit stuff

--- plot/plot_colour_SLSN.py
+++ plot/plot_colour_SLSN.py
@@ -11,12 +11,14 @@ slsn_list = [1279517., 1279780., 1281880., 1290780., 1298914.,
              1320152., 1336197., 1340054., 1370309., 1372087.,
              1385229., 1562075.]
 
+def line(y):
+    return (y - 22.0) / 2
+
 for folder in ['SLSN_99']: #, 'SLSN_90', 'SLSN_50']:
     SLSN = np.loadtxt('/Users/szymon/Dropbox/'+folder+'.csv', unpack=True)
     engine = create_engine('postgresql://szymon:supernova@localhost:5432/thesis')
 
     for snid in SLSN:
-        print(snid)
         query = "SELECT * FROM real_des_interp_46 WHERE snid="+str(snid)
         df = query_localdb(query)
         df['mag'] = 31.4 - 2.5 * np.log10(df['flux'])
@@ -40,5 +42,8 @@ for folder in ['SLSN_99']: #, 'SLSN_90', 'SLSN_50']:
 
         ax.plot(g_mag[mask] - r_mag[mask],
                 g_mag[mask], c=col)
+
+        if sum(g_mag[mask] - r_mag[mask]) > line(g_mag[mask])) > 0:
+            print(snid)
 
 plt.savefig('/Users/szymon/Dropbox/Plots/SLSN_Colours.pdf', bbox_inches='tight')
