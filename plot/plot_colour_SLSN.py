c87f0f7a149602aa10bf739e28c9f19154f74e87
Szymon Prajs <S.Prajs@soton.ac.uk>
2018-06-16 20:07:43 +0100

    Make a colour plot of mags

--- plot/plot_colour_SLSN.py
+++ plot/plot_colour_SLSN.py
@@ -16,15 +16,20 @@ for folder in ['SLSN_99']: #, 'SLSN_90', 'SLSN_50']:
         print(snid)
         query = "SELECT * FROM real_des_interp_46 WHERE snid="+str(snid)
         df = query_localdb(query)
+        df['mag'] = 2.5 * np.log10(df['flux']) - 31.4
 
-        season = df['season'][df['flux'] == df['flux'].max()].values[0]
+        season = df['season'][df['mag'] == df['mag'].min()].values[0]
         df = df[df['season'] == season]
         gdf = df.groupby('band')
         g = gdf.get_group('g')
         z = gdf.get_group('z')
-        g_mag = np.log10(g['flux'].values)
-        g_z_mag = np.log10(g['flux'].values / z['flux'].values)
-        ax.plot(g_z_mag,
-                g_mag, c='black')
+        g_mag = g['mag'].values
+        z_mag = z['mag'].values
+        g_mask = g_mag < 26
+        z_mask = z_mag < 26
+        mask = g_mask * z_mask
+
+        ax.plot(g_mag[mask] / z_mag[mask],
+                g_mag[mask], c='black')
 
 plt.savefig('/Users/szymon/Dropbox/Plots/SLSN_Colours.pdf', bbox_inches='tight')
