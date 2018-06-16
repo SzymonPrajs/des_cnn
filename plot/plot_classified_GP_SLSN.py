04a47f7c81271ab3ce368525e2ff7d54a4634fdf
Szymon Prajs <S.Prajs@soton.ac.uk>
2018-06-16 11:04:13 +0100

    Make gp plots

---
+++ plot/plot_classified_GP_SLSN.py
@@ -0,0 +1,21 @@
+import sys
+import numpy as np
+import pandas as pd
+import tools.des_tools as des
+import matplotlib.pyplot as plt
+from sqlalchemy import create_engine
+from tools.des_tools import query_localdb
+from tools.plot_des import plot_all_seasons
+
+
+for folder in ['SLSN_99', 'SLSN_90', 'SLSN_50']:
+    SLSN = np.loadtxt('/Users/szymon/Dropbox/'+folder+'.csv', unpack=True)
+    engine = create_engine('postgresql://szymon:supernova@localhost:5432/thesis')
+
+    for snid in SLSN:
+        query = "SELECT * FROM real_des_interp_46 WHERE snid="+str(snid)
+        df = query_localdb(query)
+        print('snid:', df['snid'][0])
+
+        ax = plot_all_seasons(df)
+        plt.savefig('/Users/szymon/Dropbox/Plots/GP_'+folder+'/'+str(snid)+'.png', bbox_inches='tight')
